e16d7ffea3211b1da1bb8b6c15964b035d74d92f
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-04 13:09:30 +0100

    Hash user password using bcrypt

--- src/db/user.js
+++ src/db/user.js
@@ -1,10 +1,13 @@
+const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 
 async function create(value) {
+    const salt = await bcrypt.genSalt(10);
+
     return await new User({
         name: value.name,
         email: value.email,
-        password: value.password,
+        password: await bcrypt.hash(value.password, salt),
     }).save();
 }
 
