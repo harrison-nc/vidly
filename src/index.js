4cd230975b91a6d9f9988e0fa5a84b425f7c4d5f
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-05 03:17:48 +0100

    Refactor index.js move config functions to separate module

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ Joi.objectId = require('joi-objectid')(Joi);
 const config = require('config');
 const express = require('express');
 
-require('./startup/logging');
+require('./startup/logging')();
+require('./startup/config')();
 const db = require('./startup/db');
 const routes = require('./startup/routes');
-
-if (!config.get('jwtPrivateKey')) {
-    console.log('FATAL ERROR: jwtPrivateKey not defined.');
-    process.exit(1);
-}
-
-if (!config.get('db.url')) {
-    console.log('FATAL ERROR: Database url not defined.');
-    process.exit(1);
-}
 
 async function main() {
     const app = express();
---
+++ src/startup/config.js
@@ -0,0 +1,11 @@
+const config = require('config');
+
+module.exports = function () {
+    if (!config.get('jwtPrivateKey')) {
+        throw new Error('FATAL ERROR: jwtPrivateKey not defined.');
+    }
+
+    if (!config.get('db.url')) {
+        throw new Error('FATAL ERROR: Database url not defined.');
+    }
+}
