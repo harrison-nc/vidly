533e3aa7b142588b932ab4e8cbf7eaef66d03d3c
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-05 02:42:19 +0100

    Refactor index.js move routes to separate modules

--- src/index.js
+++ src/index.js
@@ -6,13 +6,8 @@ const genres = require('./routes/genres');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
 const express = require('express');
+const routes = require('./startup/routes');
 
 winston.handleExceptions(new winston.transports.File({
     filename: config.get('log.exfilename')
@@ -43,7 +38,6 @@ if (!config.get('db.url')) {
 }
 
 const app = express();
-app.use(express.json());
 
 async function main() {
     await mongoose.connect(config.get('db.url'), {
@@ -54,13 +48,7 @@ async function main() {
 
     console.log('Connected to database.');
 
-    app.use('/api/genres', genres);
-    app.use('/api/customers', customers);
-    app.use('/api/movies', movies);
-    app.use('/api/rentals', rentals);
-    app.use('/api/users', users);
-    app.use('/api/logins', auth);
-    app.use(error);
+    routes(app);
 
     const port = parseInt(config.get('db.port')) || 3000;
     app.listen(port, () => console.log(`Listening on port ${port}...`));
