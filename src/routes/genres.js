c510e5770fe0d2c72c006f6f62d449b0164f11d2
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-08 01:45:30 +0100

    Test GET /api/genres/:id

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -2,6 +2,7 @@ const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const express = require('express');
 const { create, update, remove, getAll, get, validate } = require('../db/genre');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
@@ -13,6 +14,9 @@ router.get('/', async (req, res) => {
 
 // Get a genre using id
 router.get('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+        return res.status(404).send('Invalid ID.');
+
     const genre = await get(req.params.id);
     if (!genre) return res.status(404).send("The genre with the given id was not found.");
 
