83674631ae6e6c448fb221cef7e3a0a9286aa853
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-09 17:31:20 +0100

    Use auth middlware to protect /api/returns POST route

--- src/routes/returns.js
+++ src/routes/returns.js
@@ -1,10 +1,9 @@
+const auth = require('../../src/middleware/auth');
 const express = require('express');
 const router = express.Router();
 const { Rental } = require('../models/rental');
 
-router.post('/', async (req, res) => {
-    if (!req.headers['x-auth-token']) return res.status(401).send('Unauthorized!');
-
+router.post('/', auth, async (req, res) => {
     if (!req.body.customerId) return res.status(400).send('customerId not provided.');
     if (!req.body.movieId) return res.status(400).send('movieId not provided.');
 
