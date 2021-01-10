54e994bf2b1783a3d3f703be5f5664233947ed73
Harrison Chikwe <nhcdeveloper@gmail.com>
2021-01-10 14:13:26 +0100

    Refactor returns to use functions from rental model

--- src/db/rental.js
+++ src/db/rental.js
@@ -1,5 +1,7 @@
+const moment = require('moment');
 const Fawn = require('fawn');
 const mongoose = require('mongoose');
+const { Movie } = require('../models/movie');
 const { Rental, validate } = require('../models/rental');
 
 const Task = Fawn.Task;
@@ -46,8 +48,33 @@ async function removeRental(rentalId) {
     return await Rental.findByIdAndRemove(rentalId, { useFindAndModify: false });
 }
 
+function lookup(customerId, movieId) {
+    return Rental.findOne({
+        'customer._id': customerId,
+        'movie._id': movieId,
+    });
+}
+
+async function returnRental(rental) {
+    rental.dateReturned = Date.now();
+    const rentalDays = moment().diff(rental.dateOut, 'days');
+    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
+    const updatedRental = await rental.save();
+
+    const movie = await Movie.findByIdAndUpdate(rental.movie._id, {
+        $inc: { numberInStock: 1 }
+    });
+
+    await movie.save();
+
+    return updatedRental;
+}
+
+
 exports.create = createRental;
 exports.remove = removeRental;
 exports.getAll = getRentals;
 exports.get = getRental;
+exports.lookup = lookup;
+exports.returnRental = returnRental;
 exports.validate = validate;
--- src/routes/returns.js
+++ src/routes/returns.js
@@ -1,17 +1,14 @@
-const moment = require('moment');
 const express = require('express');
 const router = express.Router();
-
 const auth = require('../../src/middleware/auth');
 const validator = require('../middleware/validateRequestParameters');
-const { Rental } = require('../models/rental');
-const { Movie } = require('../models/movie');
-const { validate: validateReturn } = require('../models/returns');
+const { lookup, returnRental, validate: validateRental } = require('../db/rental');
 
-const validate = validator(validateReturn);
+const validateParams = validator(validateRental);
 
-router.post('/', [auth, validate], async (req, res) => {
-    let rental = await get(req.body.customerId, req.body.movieId)
+router.post('/', [auth, validateParams], async (req, res) => {
+    let rental = await lookup(req.body.customerId, req.body.movieId)
+
     if (!rental) return res.status(404).send('Rental not found for the given customer and movie');
 
     if (rental.dateReturned) return res.status(400).send('Return already processed.');
@@ -20,27 +17,5 @@ router.post('/', [auth, validate], async (req, res) => {
 
     res.status(200).send(rental);
 });
-
-function get(customerId, movieId) {
-    return Rental.findOne({
-        'customer._id': customerId,
-        'movie._id': movieId,
-    });
-}
-
-async function returnRental(rental) {
-    rental.dateReturned = Date.now();
-    const rentalDays = moment().diff(rental.dateOut, 'days');
-    rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
-    const updatedRental = await rental.save();
-
-    const movie = await Movie.findByIdAndUpdate(rental.movie._id, {
-        $inc: { numberInStock: 1 }
-    });
-
-    await movie.save();
-
-    return updatedRental;
-}
 
 module.exports = router;
